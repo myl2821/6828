a0dc47d33270c65a738bac55a9b06080ddf7e7cc
myl2821 <myl2821@gmail.com>
2015-05-24 00:03:22 +0800

    Lab 5: Implement kernel-lvl fs operation.

--- fs/fs.c
+++ fs/fs.c
@@ -62,7 +62,15 @@ alloc_block(void)
 	// super->s_nblocks blocks in the disk altogether.
 
 	// LAB 5: Your code here.
-	panic("alloc_block not implemented");
+	uint32_t blockno;
+	for(blockno = 0; blockno < super->s_nblocks; blockno++) {
+		if (block_is_free(blockno)) {
+			bitmap[blockno/32] &= ~(1<<(blockno%32));
+			flush_block(&bitmap[blockno/32]);
+			return blockno;
+		}
+	}
+	
 	return -E_NO_DISK;
 }
 
@@ -134,8 +142,30 @@ fs_init(void)
 static int
 file_block_walk(struct File *f, uint32_t filebno, uint32_t **ppdiskbno, bool alloc)
 {
-       // LAB 5: Your code here.
-       panic("file_block_walk not implemented");
+	// LAB 5: Your code here.
+	if(filebno >= NDIRECT + NINDIRECT)
+		return -E_INVAL;
+
+	if(filebno < NDIRECT) {
+		*ppdiskbno = &f->f_direct[filebno];
+		return 0;
+	}
+	filebno -= NDIRECT;
+
+	if(f->f_indirect == 0){
+		if(!alloc)
+			return -E_NOT_FOUND;
+		
+		uint32_t blockno;
+		if((blockno = alloc_block()) < 0)
+			return -E_NO_DISK;
+		memset(diskaddr(blockno), 0, BLKSIZE);
+		f->f_indirect = blockno;
+	}
+
+	uint32_t *indrct_blk = diskaddr(f->f_indirect);
+	*ppdiskbno = &indrct_blk[filebno];
+	return 0;
 }
 
 // Set *blk to the address in memory where the filebno'th
@@ -149,8 +179,20 @@ file_block_walk(struct File *f, uint32_t filebno, uint32_t **ppdiskbno, bool all
 int
 file_get_block(struct File *f, uint32_t filebno, char **blk)
 {
-       // LAB 5: Your code here.
-       panic("file_get_block not implemented");
+	// LAB 5: Your code here.
+	int ret;
+	uint32_t *blkno;
+	if((ret = file_block_walk(f, filebno, &blkno, 1)) < 0)
+		return ret;
+
+	if(!(*blkno)) {
+		if((ret = alloc_block()) < 0)
+			return ret;
+		*blkno = ret;
+	}
+
+	*blk = (char *)diskaddr(*blkno);
+	return 0;
 }
 
 // Try to find a file named "name" in dir.  If so, set *file to it.
